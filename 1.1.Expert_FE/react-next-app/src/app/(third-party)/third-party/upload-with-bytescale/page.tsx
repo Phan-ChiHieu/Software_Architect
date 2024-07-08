c01190762e1e4351e1aa43f2a6ac7862a90c6d37
EdricPhan <edricphan.dev@gmail.com>
2024-07-08 11:40:30 +0700

    update code uppload with bytescale and remove env

---
+++ 1.1.Expert_FE/react-next-app/src/app/(third-party)/third-party/upload-with-bytescale/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import PageBytescaleClient from "./page-client";
+
+export default function PageBytescale() {
+  return <PageBytescaleClient />;
+}
---
+++ 1.1.Expert_FE/react-next-app/src/app/(third-party)/third-party/upload-with-bytescale/readme.md
@@ -0,0 +1,4 @@
+# Upload pdf, csv, json and images fast with bytescale
+
+    - link: https://www.bytescale.com/
+    - docs: https://www.bytescale.com/dashboard/docs
